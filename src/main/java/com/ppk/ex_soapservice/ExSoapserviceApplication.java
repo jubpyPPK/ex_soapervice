1d80c8fa52ec0e654f3168fd1a7236f4ebad4e3f
Prapakorn <prapakorn.jar@ascendcorp.com>
2018-04-05 15:08:02 +0700

    Initial project.

---
+++ src/main/java/com/ppk/ex_soapservice/ExSoapserviceApplication.java
@@ -0,0 +1,12 @@
+package com.ppk.ex_soapservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExSoapserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExSoapserviceApplication.class, args);
+	}
+}
